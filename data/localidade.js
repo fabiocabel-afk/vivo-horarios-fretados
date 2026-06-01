b91e8cef911ac527e87b3e0622fe33321f8837d5
fabiocabel-afk <fabiocabel@gmail.com>
2026-05-31 23:55:46 -0300

    Update localidade.js

--- data/localidade.js
+++ data/localidade.js
@@ -2,26 +2,17 @@ const locais = [
   {
     nome: "BORBA GATO",
     endereco: "Rua do Estilo Barroco, 318",
-    coordenadas: "-23.632914,-46.6934888",
-    latitude: -23.632914,
-    longitude: -46.6934888
+    coordenadas: "@-23.63299,-46.6935492,3a,75y,73.98h,90.6t"
   },
   {
-    nome: "GRANJA JULIETA",
-    endereco: "Rua Alexandre Dumas, 4403",
-    coordenadas: "-23.627820,-46.711355",
-    latitude: -23.627820,
-    longitude: -46.711355
+    nome: "MORUMBI",
+    endereco: "Av. das Nações Unidas, 14261",
+    coordenadas: "@-23.6220347,-46.7014569,3a,75y,74.2h,94.12t"
   },
   {
     nome: "TELEFONICA",
     endereco: "Rua Antônio das Chagas, 1196",
-    coordenadas: "-23.6291001,-46.7042669",
-    latitude: -23.6291001,
-    longitude: -46.7042669
+    coordenadas: "@-23.6289517,-46.7045004,3a,75y,92.98h,83.58t"
   }
 ];
 
-// Exemplo de como acessar:
-console.log(locais[0].nome); // Saída: BORBA GATO
-console.log(locais[0].latitude); // Saída: -23.632914
